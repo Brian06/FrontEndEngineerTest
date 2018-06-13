8eeb7fc582a5a0243e2acfba23f8c42aa31797f4
Unknown <bsalazarsanchez06@gmail.com>
2018-06-13 00:19:17 -0600

    Configure the first components
    Using react-bootstrap for the responsive design and react-router v4 for the routing

---
+++ src/components/signUp/signUp.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>SignUp</h3>
+      </div>
+    )
+  }
+
+}
+
+export default SignUp;
