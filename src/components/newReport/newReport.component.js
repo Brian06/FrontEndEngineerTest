a64256c4b0153903931f3ea1c7d5ca254cf3c017
Unknown <bsalazarsanchez06@gmail.com>
2018-06-15 19:41:15 -0600

    Add some UI fro the newReport component

--- src/components/newReport/newReport.component.js
+++ src/components/newReport/newReport.component.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { FormGroup, ControlLabel, FormControl, Button, Form } from 'react-bootstrap';
+import axios from 'axios';
 
 class NewReport extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      redirect: false
+    };
 
+    this.handleInputChange = this.handleInputChange.bind(this);   
+    this.search = this.search.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  search() {
+    const email = this.state.email;
+    axios.get(`https://www.beenverified.com/hk/dd/email?email=${email}&exporttype=jsonp`)
+    .then(response => {
+      let data = response.data;
+      //this.setState({ projects:data, loading: false });
+    })
+    .catch(error => {
+      this.setState({ loading: false });
+    });
+  }
+  
   render() {
     return (
       <div>
-        <h3>New Report</h3>
+        <Form inline>
+        <h2>Add A New Report</h2>
+        <h5>Enter an email address to searh information of the user!</h5>
+          <FormGroup controlId="formInlineEmail">
+            <ControlLabel>Email to Search: </ControlLabel>{' '}
+            <FormControl 
+              placeholder="jane.doe@example.com" 
+              name="email"
+              value={ this.state.email }
+              onChange={this.handleInputChange} 
+            />
+          </FormGroup>{' '}
+          <Button onClick={ this.search }>Search Report</Button>
+        </Form>
       </div>
     )
   }
